fa502272d22ea6bc9a8bd715b2cf330424d6be40
Dirk Lemstra <dirk@lemstra.org>
2021-01-22 08:34:51 +0100

    Also try to find bash in the %PATH%.

--- CloneRepositories.cmd
+++ CloneRepositories.cmd
@@ -9,7 +9,16 @@ if not "%2"=="" set TYPE=%2
 set BASH="%PROGRAMFILES%\Git\bin\bash.exe"
 if exist %BASH% goto EXECUTE
 
-set bash="%PROGRAMFILES(x86)%\Git\bin\bash.exe"
+set BASH="%PROGRAMFILES(x86)%\Git\bin\bash.exe"
+if exist %BASH% goto EXECUTE
+
+set BASH="%ProgramW6432%\Git\bin\bash.exe"
+if exist %BASH% goto EXECUTE
+
+set BASH="%USERPROFILE%\scoop\apps\git\current\bin\bash.exe"
+if exist %BASH% goto EXECUTE
+
+for /F "tokens=*" %%g in ('where bash') do (SET BASH=%%g)
 if exist %BASH% goto EXECUTE
 
 echo Failed to find bash.exe
